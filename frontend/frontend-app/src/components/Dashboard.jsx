369a91050179f828a8071af9a9e9008509c61b68
LOHITHA B E <138787030+LOHITHABE@users.noreply.github.com>
2026-02-06 14:06:37 +0530

    Update Dashboard.jsx

--- frontend/frontend-app/src/components/Dashboard.jsx
+++ frontend/frontend-app/src/components/Dashboard.jsx
@@ -21,7 +21,7 @@ export default function Dashboard() {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/analyze", {
+      const response = await fetch("https://financial-health-assessment-5z2r.onrender.com/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
